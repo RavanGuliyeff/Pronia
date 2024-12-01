fb6037232243d49dd32bde4c5e32e31d3913457d
RavanGuliyeff <guliyevtr-ab107@code.edu.az>
2024-12-01 14:25:12 +0400

    FrontToBack Update

--- ProniaWebApp/obj/Debug/net8.0/ProniaWebApp.AssemblyInfo.cs
+++ ProniaWebApp/obj/Debug/net8.0/ProniaWebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProniaWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0613bdae2b1ca4bb5b4354a9962fbb1703fbd7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProniaWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProniaWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
