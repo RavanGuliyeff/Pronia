a0dbb38f0fe35f5e94ae49f83fba966297eb62d7
RavanGuliyeff <guliyevtr-ap116@code.edu.az>
2024-12-02 11:45:57 +0400

    Image Start

--- ProniaWebApp/Areas/Manage/ViewModels/Product/CreateProductVm.cs
+++ ProniaWebApp/Areas/Manage/ViewModels/Product/CreateProductVm.cs
@@ -8,7 +8,8 @@ namespace ProniaWebApp.Areas.Manage.ViewModels.Product
 		public string? Description { get; set; }
 		public string SKU { get; set; }
 		public double Price { get; set; }
-		//public List<int>? ImageIds { get; set; }
+		public IFormFile MainPhoto { get; set; }
+		public List<IFormFile>? Images { get; set; }
 		public List<int> CategoryIds { get; set; }
 		public List<int> TagIds { get; set; }
 	}
